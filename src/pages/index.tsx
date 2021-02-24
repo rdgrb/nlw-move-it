78c5802485ba09170efcf7f9af832f1be7797feb
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-02-24 19:19:07 -0300

    Adicionado title e favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,15 @@ import { Profile } from "../components/Profile";
 
 import styles from "../styles/pages/Home.module.css";
 
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>moveit | Dashboard</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
