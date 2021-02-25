333e797448709e0c486d4600f737a5de09e14ec6
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-02-25 18:16:14 -0300

    Adicionado exibição de level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+    
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/rdgrb.png" alt="Foto de perfil" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Rodrigo Ribeiro</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level icon" />
-                    Level 1
+                    Level { level }
                 </p>
             </div>
         </div>
