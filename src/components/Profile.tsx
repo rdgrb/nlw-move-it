001598614c59f26c04e542763c8016a20b6cf228
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2021-02-26 20:37:40 -0300

    Adicionado botão para troca de temas (claro - escuro)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,28 @@
 import { useContext } from "react";
 import { ChallengesContext } from "../contexts/ChallengesContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 import styles from "../styles/components/Profile.module.css";
 
+import { Moon } from "../components/icons/Moon";
+
 export function Profile() {
+    const { theme, changeTheme } = useContext(ThemeContext);
     const { level } = useContext(ChallengesContext);
     
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/rdgrb.png" alt="Foto de perfil" />
+            <img src="icons/default-avatar.svg" alt="Foto de perfil" />
             <div>
-                <strong>Rodrigo Ribeiro</strong>
+                <strong>Convidado</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level icon" />
                     Level { level }
                 </p>
+            </div>
+            <div className={styles.buttonContainer}>
+                <button onClick={changeTheme}>
+                    <Moon color={ theme.title === "light" ? "#000000" : "#FFFFFF"}/>
+                </button>
             </div>
         </div>
     )
